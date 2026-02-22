1fb39af22b41b39a13c6bcdcba4f50f1edb00de7
Md Firdous <firdousmd000@gmail.com>
2026-02-23 02:42:58 +0530

    Create Function.py

---
+++ Function.py
@@ -0,0 +1,59 @@
+# function :- function is a block of code that run when we call
+
+# function definition:- function definitition is define with function name with parenthesesis ()
+
+# def :- it is a reserve keyword that is used for create the function
+
+# parameter:- inside function definition that you are passing any king of variable that is known as parameter
+# parameter scope is always local means parameter only we can access inside function body
+# line of code written inside vertically same line :- function body
+
+# reserve keyword that is used for pass function body data outside a function it return only one value ata time
+# return terminate the function body 
+
+# function call:- for run function body code we are calling function with function name we can call multiple time same function
+
+# Arguments:- during calling the function if we are passing any real value that is known as Arguments
+# we can pass as arguments anything  like string , integer , function , variable 
+
+# note:- python support positional arguments 
+
+# recursion function :- when a function call itself is known as recursion function
+
+# constructor function :- it is a kind of function that is run automatically during the creation of object 
+
+# higherorder function:- if a function you are passing as a arguments in another function that is known as higherorder function 
+
+# step1: def  my_file(a,b):
+            #     block of Code 
+            #     , 
+            #      ..
+            #      ..
+                 
+            #     return 
+        
+            # my_file(10,20)
+
+def shadi(a,b):
+    print(a)
+    print(b)
+    return
+    print("this will not display data")
+shadi(10,20)
+
+
+def add():
+    n1=100
+    n2=200
+    s=n1+n2
+    print("sum of two n0 = ",s)
+def greet(name):
+    print(f"{name} good Morning! ")
+
+def fee(paisa):
+    v=f"aapko itna paisa jama krna h {paisa} "
+    return v 
+    print(v)  # ye run nhi hota h return ke baad h isiliye
+    
+     
+
