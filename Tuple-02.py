10d260e966ed5117f9de0a888b50e1702aa3a787
Md Firdous <firdousmd000@gmail.com>
2026-02-23 02:15:19 +0530

    Create Tuple-02.py with tuple examples
    Added a Python script demonstrating tuple characteristics and operations.

---
+++ Tuple-02.py
@@ -0,0 +1,57 @@
+# Tuple :- 
+# -> Tuple is exactly same as list except mutability 
+# tuple is a immutable means we cannot add and delete data inside tuple but we can access data 
+
+# 1.non primitive data type
+# 2.tuple support indexing
+# 3.it store homogenous and heterogenous data type 
+# 4.it support slicing 
+# 5.by using () we can create tuple but it is optional 
+# 6.we can store multiple data separated by ,
+# 7.it support duplicate value
+
+# a=10,  # tuple 
+# print(type(a))
+# b=(20)  # integer
+# print(type(b))
+
+# name="mohan", 
+# print(type(name))
+
+# v="mohan"  # string 
+
+# how to access data from tuple
+v=(100,200,300,400)
+# index method
+# print(v[0])
+# for i in v:
+#     print(i,end="")
+# slicicng
+print(v[1:4:1])
+
+
+# lg=len(v)
+# i=0
+# while i<lg:
+#     print(v[i])
+#     i=i+1
+
+# tuple packing and unpacking
+# packing
+# a=10
+# n=20
+# c=30
+# d=50
+# t=a,n,c,d  
+# print(t) 
+
+# # unpacking
+# m=100,200,300,400,500
+# n1,n2,n3,n4,n5=m
+# print(n1)
+# print(n2)
+# print(n3)
+# print(n4)
+# print(n5)
+ 
+ 
